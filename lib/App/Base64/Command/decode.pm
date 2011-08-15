85ef402429e974f997e08da985f370057488c9f0
Glenn Fowler <cebjyre@cpan.org>
2011-08-15 10:33:16 +0000

    Import of CEBJYRE/App-Base64-v0.0.1 from CPAN.
    gitpan-cpan-distribution: App-Base64
    gitpan-cpan-version:      v0.0.1
    gitpan-cpan-path:         CEBJYRE/App-Base64-v0.0.1.tar.gz
    gitpan-cpan-author:       CEBJYRE
    gitpan-cpan-maturity:     released

---
+++ lib/App/Base64/Command/decode.pm
@@ -0,0 +1,46 @@
+use strict;
+use warnings;
+
+package App::Base64::Command::decode;
+{
+  $App::Base64::Command::decode::VERSION = 'v0.0.1';
+}
+
+# ABSTRACT: Handle the decoding side of Base64
+
+use App::Base64 -command;
+use MIME::Base64 qw(decode_base64);
+
+sub execute {
+	my $self = shift;
+	
+	while (<STDIN>)
+	{
+		print decode_base64($_);
+	}
+}
+
+__END__
+=pod
+
+=head1 NAME
+
+App::Base64::Command::decode - Handle the decoding side of Base64
+
+=head1 VERSION
+
+version v0.0.1
+
+=head1 AUTHOR
+
+Glenn Fowler <cebjyre@cpan.org>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2011 by Glenn Fowler.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
+
